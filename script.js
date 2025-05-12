137d64ac029385ea112cc8a3da15c6bbb054d355
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-12 09:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const headingEl=document.getElementById('status')
 const buttonEl=document.getElementById('enterBtn')
 buttonEl.addEventListener('click',()=> {
-	headingEl.textContent='Entered Metaverse'
+	headingEl.innerHTML='<h1>Entered Metaverse</h1>'
 })
